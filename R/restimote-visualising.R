a440407ec9ce4295d1cfb3e675cb9341bf63c535
hejtmy <hejtmy@gmail.com>
2017-12-07 03:45:52 -0800

    Adds visualisation of trials

--- R/restimote-visualising.R
+++ R/restimote-visualising.R
@@ -1,3 +1,4 @@
-plot_trial_path <- function(obj, trialID){
-  
+plot_trial_path <- function(obj, trialId){
+  df_trial_log <- get_position_trial(obj, trialId)
+  plot(df_trial_log$Position.X, df_trial_log$Position.Y, type = "l")
 }
