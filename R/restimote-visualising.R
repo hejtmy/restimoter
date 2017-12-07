dc14816d713b0ddfe4e9a9969ed2a6626dda2889
hejtmy <hejtmy@gmail.com>
2017-12-07 04:49:57 -0800

    Adds plotting info

--- R/restimote-visualising.R
+++ R/restimote-visualising.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param obj RestimoteObject Needs to be preprocessed
+#' @param trialId integed of given trial
+#'
+#' @return prints plot
+#' @export 
+#'
+#' @examples
+#' plot_trial_path(obj, 1)
 plot_trial_path <- function(obj, trialId){
   df_trial_log <- get_position_trial(obj, trialId)
   plot(df_trial_log$Position.X, df_trial_log$Position.Y, type = "l")
