f056a9960957279b5dec25bc955b040b4d6ec2d3
hejtmy <hejtmy@gmail.com>
2018-03-01 06:06:40 -0800

    Fixes the trial duration generic

--- R/restimote-generics.R
+++ R/restimote-generics.R
@@ -1,13 +1,13 @@
 #' Returns how long the trial took and removes potential pauses in the log
 #'
 #' @param obj 
-#' @param withoutPauses Defaults to true
+#' @param without_pauses Defaults to true
 #' @param ... 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-get_trial_duration <- function(obj, trialId, withoutPauses = T, ...){
+get_trial_duration <- function(obj, trialId, without_pauses = T, ...){
   UseMethod("get_trial_duration")
 }
