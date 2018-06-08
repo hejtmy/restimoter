b5febf912527d64004d87013c79f91e91eb46007
hejtmy <hejtmy@gmail.com>
2018-06-07 19:26:05 -0700

    Fixes searching for true trial log

--- R/restimote-cleaning.R
+++ R/restimote-cleaning.R
@@ -37,7 +37,6 @@ smooth_positions <- function(log, type = "median", points = 11){
 remove_random_points <- function(df_player, allowed_speed = 4){
   
 }
-
 #' Goes through the restimote log and defines trials not in respect to the 
 #' companion log but in respect to when player achieved started at the start 
 #' and when they reached given goal - times are incorrect, but path shoudl correspond
@@ -52,11 +51,11 @@ remove_random_points <- function(df_player, allowed_speed = 4){
 #'
 #' @examples
 true_trial_log <- function(obj, trialId, benevolence = 30, radius = 2){
- ls <- find_true_trial_log(obj, trialId, benevolence, radius)
+ ls <- create_true_trial_log(obj, trialId, benevolence, radius)
  return(ls$log)
 }
 
-find_true_trial_log <- function(obj, trialId, benevolence = 30, radius = 2){
+create_true_trial_log <- function(obj, trialId, benevolence = 30, radius = 2){
   start_pos <- get_start_position(obj, trialId)
   goal_pos <- get_goal_position(obj, trialId)
   # positions are shifted to the later times ... so we know that the trial definitely starte at 
@@ -68,18 +67,36 @@ find_true_trial_log <- function(obj, trialId, benevolence = 30, radius = 2){
   small_log <- get_log_timewindow.restimote(obj, timewindow$start, timewindow$end + benevolence)
   distances_start <- apply(small_log[, c('Position.X', "Position.Y")], 1, function(x) navr::euclid_distance(x, start_pos))
   distances_goal <- apply(small_log[, c('Position.X', "Position.Y")], 1, function(x) navr::euclid_distance(x, goal_pos))
+  
   i_close_to_start <- NA
   i_close_to_goal <- NA
-  found_radius <- 0
-  for(found_radius in seq(0.5, radius, 0.1)){
-    if(is.na(i_close_to_start)) i_close_to_start <- which(distances_start < found_radius)[1]
-    if(is.na(i_close_to_goal)) i_close_to_goal <- which(distances_goal < found_radius)[1]
+  goal_radius <- NA
+  start_radius <- NA
+  
+  for(rad in seq(0.5, radius, 0.1)){
+    if(is.na(i_close_to_start)){
+      i_close_to_start <- which(distances_start < rad)[1]
+      start_radius <- rad
+    }
+    if(is.na(i_close_to_goal)){
+      i_close_to_goal <- which(distances_goal < rad)[1]
+      goal_radius <- rad
+    }
   }
-  if(any(is.na(c(i_close_to_goal, i_close_to_start)))){
+  if(any(is.na(c(i_close_to_goal, i_close_to_start)))){zzzz
     print("wasn't able to find a goal within such radius")
     return(NULL)
   }
   found_log <- small_log[i_close_to_start:i_close_to_goal, ]
+  
+  # changing first and last point to start and goal
+  found_log[1,"Position.X"] <- start_pos[1]
+  found_log[1, "Position.Y"] <- start_pos[2]
+  found_log[nrow(found_log), "Position.X"] <- goal_pos[1]
+  found_log[nrow(found_log), "Position.Y"] <- goal_pos[2]
+  
+  # recalculating distance
+  
   found_benevolence <- small_log$Time[i_close_to_goal] - timewindow$end
-  return(list(log = found_log, radius = found_radius, benevolence = found_benevolence))
+  return(list(log = found_log, goal_radius = goal_radius, start_radius = start_radius, benevolence = found_benevolence))
 }
